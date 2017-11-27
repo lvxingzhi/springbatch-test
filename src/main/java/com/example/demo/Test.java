3c5fb1e88f897938f09c00d456ba9d78ec07f88d
lvxz5 <lvxz5@ziroom.com>
2017-11-27 11:42:41 +0800

    x

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+/**
+ * <p></p>
+ * <p>
+ * <PRE>
+ * <BR>	修改记录
+ * <BR>-----------------------------------------------
+ * <BR>	修改日期			修改人			修改内容
+ * </PRE>
+ *
+ * @author lvxz5
+ * @version 1.0
+ * @date 2017/10/19
+ * @since 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
